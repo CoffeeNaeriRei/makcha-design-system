54ff063edd6ace24fd60b572b61edffb0ea79fed
yuncoffee <dong072815@gmail.com>
2024-05-05 20:37:26 +0900

    Initial Commit

---
+++ Tests/makcha-design-systemTests/makcha_design_systemTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import makcha_design_system
+
+final class makcha_design_systemTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
