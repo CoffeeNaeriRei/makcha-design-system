96c0047e8e2d012a96a4d64098ca56e25a4763cb
yuncoffee <dong072815@gmail.com>
2024-05-11 22:27:17 +0900

    [add] Transport Color 및 타입 추가 #6

---
+++ Sources/MakchaDesignSystem/Foundation/Color/TransportColors.swift
@@ -0,0 +1,126 @@
+//
+//  File.swift
+//
+//
+//  Created by yuncoffee on 5/11/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension TransportType {
+    var color: Color {
+        switch self {
+        case .subway(let subwayType):
+            subwayType.color
+        case .bus(let busType):
+            busType.color
+        }
+    }
+}
+    
+
+extension MetropolitanRailway {
+    var color: Color {
+        switch self {
+        case .line1:
+            Color(.line1)
+        case .line2:
+            Color(.line2)
+        case .line3:
+            Color(.line3)
+        case .line4:
+            Color(.line4)
+        case .line5:
+            Color(.line5)
+        case .line6:
+            Color(.line6)
+        case .line7:
+            Color(.line7)
+        case .line8:
+            Color(.line8)
+        case .line9:
+            Color(.line9)
+        case .공항철도:
+            .purple
+        case .신분당선:
+            .purple
+        case .수인분당선:
+            .purple
+        case .경강선:
+            .purple
+        case .경의중앙선:
+            .purple
+        case .경춘선:
+            .purple
+        case .서해선:
+            .purple
+        case .인천1호선:
+            .purple
+        case .인천2호선:
+            .purple
+        case .신림선:
+            .purple
+        case .우이신설선:
+            .purple
+        case .김포골드라인:
+            .purple
+        case .용인에버라인:
+            .purple
+        case .의정부경전철:
+            .purple
+        case .gtxA:
+            .purple
+        }
+    }
+}
+
+extension SeoulBusType {
+    var color: Color {
+        switch self {
+        case .공항:
+            .purple
+        case .광역:
+            .purple
+        case .간선:
+            .purple
+        case .지선:
+            .purple
+        case .맞춤:
+            .purple
+        case .순환:
+            .purple
+        case .심야:
+            .purple
+        case .마을:
+            .purple
+        case .투어:
+            .purple
+        }
+    }
+}
+
+extension GyeonggiBusType {
+    var color: Color {
+        switch self {
+        case .프리미엄:
+            .purple
+        case .경기순환:
+            .purple
+        case .직행좌석:
+            .purple
+        case .좌석:
+            .purple
+        case .일반:
+            .purple
+        case .마을:
+            .purple
+        }
+    }
+}
+
+public extension Color {
+    static func transport(_ type: TransportType) -> Self {
+        type.color
+    }
+}
---
+++ Sources/MakchaDesignSystem/Utils/Transport.swift
@@ -0,0 +1,72 @@
+//
+//  File.swift
+//  
+//
+//  Created by yuncoffee on 5/11/24.
+//
+
+import Foundation
+import SwiftUI
+
+public protocol Transportable {}
+extension Transportable {
+    var color: Color { .blue }
+}
+public protocol SubwayType: Transportable {}
+public protocol BusType: Transportable {}
+
+public enum TransportType {
+    case subway(SubwayType)
+    case bus(BusType)
+}
+
+// MARK: 지하철 구분에 대한 고민이 필요해보임.
+public enum MetropolitanRailway: SubwayType {
+    // --- --- //
+    case line1
+    case line2
+    case line3
+    case line4
+    case line5
+    case line6
+    case line7
+    case line8
+    case line9
+    // ---  --- //
+    case 공항철도
+    case 신분당선
+    case 수인분당선
+    case 경강선
+    case 경의중앙선
+    case 경춘선
+    case 서해선
+    case 인천1호선
+    case 인천2호선
+    case 신림선
+    case 우이신설선
+    case 김포골드라인
+    case 용인에버라인
+    case 의정부경전철
+    case gtxA
+}
+
+public enum SeoulBusType: BusType {
+    case 공항
+    case 광역
+    case 간선
+    case 지선
+    case 맞춤
+    case 순환
+    case 심야
+    case 마을
+    case 투어
+}
+
+public enum GyeonggiBusType: BusType {
+    case 프리미엄
+    case 경기순환
+    case 직행좌석
+    case 좌석
+    case 일반
+    case 마을
+}
